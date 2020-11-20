822ec7718edf0e33a9f3a7f07eef646bd6e1130a
Yohan Salamone <yohan.salamone@outlook.com>
2020-11-20 19:46:13 +0100

    meta viewport

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ if (!empty($_GET['delivery']) and $_GET['delivery'] === "sent") {
     <meta charset="utf-8"/>
     <title>MIW Party</title>
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body class="<?= $body_class ?>">
 
